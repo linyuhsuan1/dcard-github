c233ada3df022693983cd37ace88222645afb0b4
shellylin <vickey3703@gmail.com>
2023-03-12 14:34:52 +0800

    調整api

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,3 +1,3 @@
-export const GITHUB_AUTH_URL='https://linyuhsuan1.github.io/dcard-github/getAccessToken'
-export const GITHUB_SEARCH_URL = 'https://linyuhsuan1.github.io/dcard-github/search';
-export const GITHUB_REPO_ISSUE_URL = 'https://linyuhsuan1.github.io/dcard-github/repos';
+export const GITHUB_AUTH_URL='http://localhost:3001/getAccessToken'
+export const GITHUB_SEARCH_URL = 'http://localhost:3001/search';
+export const GITHUB_REPO_ISSUE_URL = 'http://localhost:3001/repos';
